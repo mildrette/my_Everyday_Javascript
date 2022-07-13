23750b15a7ffebf83d006982e47101b3b4199576
mildrette <mildredfonka1@gmail.com>
2022-07-13 13:27:40 +0100

    switch keyword

--- script.js
+++ script.js
@@ -29,3 +29,45 @@ myAgeInDogYears = earlyYears + laterYears;
 var myName = 'mildred'.toLowerCase();
 // i used interpolation here
 console.log('My name is ${myName}. i am ${myAge} years old in human years which is ${myAgeInDogYears} years old in doy years.')
+
+
+
+// switch keyword in javascript
+let userName = 'Zoey';
+userName ? console.log(`Hello, ${userName}!`)
+: console.log('Hello!');
+
+var userQuestion = 'How many childred will i have in 2015?';
+console.log(`${userName} is asking ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = '';
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain.';
+    break;
+  case 1:
+  eigthBall = 'It si decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My source say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  eightBall = 'signs point to yes';
+  break;
+  default:
+  console.log('undefined');
+}
