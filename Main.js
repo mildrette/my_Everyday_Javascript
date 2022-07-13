b98c96a9d1f26d6ade476a411a4b4675a1ea16b8
mildrette <mildredfonka1@gmail.com>
2022-07-13 14:02:41 +0100

    changed to main.js

---
+++ Main.js
@@ -0,0 +1,39 @@
+
+// switch keyword in javascript
+let userName = 'Zoey';
+userName ? console.log(`Hello, ${userName}!`)
+: console.log('Hello!');
+
+var userQuestion = 'How many childred will i have in 2015?';
+console.log(`${userName} is asking ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+let eightBall = '';
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain.';
+    break;
+  case 1:
+  eigthBall = 'It si decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My source say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  eightBall = 'signs point to yes';
+}
+
+console.log(`Magic EightBall says, ${randomNumber});
